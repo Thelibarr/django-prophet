13a3b8e8c1654eaf4ae81be02cfb24a166d650ff
Tony Larsson <tonys.mail.92@gmail.com>
2017-09-23 14:54:24 +0200

    now with psql database

--- prophet/settings.py
+++ prophet/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'prophet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sample_db',
+        'USER': 'tonylarsson',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
